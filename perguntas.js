beced88049f05d8fb76cd62cb3328285656ddfbc
gaby151515 <gabrielly.cherpinski@escola.pr.gov.br>
2025-08-05 10:48:50 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'O que é biologia?',
+    'A biologia é a ciência que estuda a vida em todas as suas formas e manifestações, desde a estrutura e função das células até a interação dos seres vivos com o meio ambiente e a evolução das espécies. É um campo amplo e diversificado, abrangendo desde o estudo de microorganismos até o de organismos complexos como os seres humanos.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é uma célula e quais são os dois principais tipos de células?',
+    'A célula é a menor unidade funcional dos seres vivos. Os dois principais tipos são: Procariontes e Eucariontes'
+)
+
+criaCartao(
+    'Veterinária',
+    'O que é a medicina veterinária',
+    'Medicina Veterinária é a área da ciência dedicada ao estudo, prevenção, diagnóstico e tratamento de doenças e lesões em animais, promovendo seu bem-estar e saúde, além de garantir a saúde pública e a segurança alimentar'
+)
+
+criaCartao(
+    'Veterinária',
+    'O que faz um médico veterinário?',
+    'Um veterinário é um profissional da área da saúde que cuida do bem-estar e da saúde dos animais, tanto domésticos quanto de produção, silvestres e exóticos. Eles previnem, diagnosticam e tratam doenças, realizam cirurgias, administram vacinas e prescrevem medicamentos.'
+)
